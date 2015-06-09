411e103fa4c148d58b95b89c57f073ce372667bc
Jonathan Claus <jonathanmclaus@gmail.com>
2015-06-09 17:20:44 -0400

    Added simple sample

--- R/reservoirSampling.R
+++ R/reservoirSampling.R
@@ -21,3 +21,27 @@ ReservoirSample <- function(data, size, inputs = AUTO, outputs = AUTO, coefficie
 
   Aggregate(data, gla, inputs, outputs)
 }
+
+SimpleSample <- function(data, size, inputs = AUTO, outputs = AUTO) {
+  inputs <- substitute(inputs)
+  check.exprs(inputs)
+  if (is.auto(inputs))
+    inputs <- convert.schema(names(data$schema))
+  inputs <- convert.exprs(inputs, data)
+
+  outputs <- substitute(outputs)
+  check.atts(outputs)
+  if (is.auto(outputs))
+    if (all(is.symbols(grokit$expressions[inputs])))
+      outputs <- unlist(lapply(grokit$expressions[inputs], as.character))
+    else
+      stop("outputs can only be AUTO when inputs are all attributes.")
+  else
+    outputs <- convert.atts(outputs)
+  if (length(outputs) != length(inputs))
+    stop("There must be exactly one output specified per input.")
+
+  gla <- GLA(statistics::Simple_Sampling, size = size)
+
+  Aggregate(data, gla, inputs, outputs)
+}
