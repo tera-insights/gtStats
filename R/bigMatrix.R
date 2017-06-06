7b5ebcd74cb32abc9a2803d17dad5077de07af3a
Alin Dobra <alin@terainsights.com>
2017-06-06 10:42:55 -0400

    Add measures support for BigMatrix

--- R/bigMatrix.R
+++ R/bigMatrix.R
@@ -1,4 +1,4 @@
-BigMatrix <- function(data, inputs, outputs, block = 40, scale = 2, diag = TRUE) {
+BigMatrix <- function(data, inputs, outputs, measures=c(correlation), block = 40, scale = 2, diag = TRUE) {
   if (missing(inputs))
     inputs <- convert.schema(data$schema)
   else
@@ -8,13 +8,18 @@ BigMatrix <- function(data, inputs, outputs, block = 40, scale = 2, diag = TRUE)
   if (length(inputs) != 2)
     stop("2 inputs expected.");
 
+  measures <- substitute(measures)
+  check.choices(measures, substitute(c(covariance, correlation, distance)))
+  numMeasures <- length(measures) - 1; ## skip function name
+
   outputs <- substitute(outputs)
   check.atts(outputs)
   outputs <- convert.atts(outputs)
-  if (length(outputs) != 3)
-    stop("3 outputs expected.")
+  if (length(outputs) != 2+numMeasures)
+    stop(2+numMeasures, " outputs expected.")
 
-  gla <- GLA(statistics::Big_Matrix, block = block, scale = scale, diag = diag)
+  gla <- GLA(statistics::Big_Matrix, measures = measures, block = block, 
+             scale = scale, diag = diag)
 
   Aggregate(data, gla, inputs, outputs)
 }
